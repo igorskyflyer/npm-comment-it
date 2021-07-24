4fa22cc4aa3e886c3478e7fc784501c46f7a09cd
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-24 20:49:51 +0200

    Add tests resources

---
+++ test/resources/results.js
@@ -0,0 +1,53 @@
+const cStyleResult = '/* hello\n\n\nworld\n\nthis is a test */'
+
+const batchResult = 'REM hello\nREM \nREM \nREM world\nREM \nREM this is a test\n'
+
+const coffeeScriptResult = '###\nhello\n\n\nworld\n\nthis is a test\n###'
+
+const dartResult = '// hello\n// \n// \n// world\n// \n// this is a test\n'
+
+const dockerFileResult = '# hello\n# \n# \n# world\n# \n# this is a test\n'
+
+const fSharpResult = '(* hello\n\n\nworld\n\nthis is a test *)'
+
+const goResult = '// hello\n// \n// \n// world\n// \n// this is a test\n'
+
+const luaResult = '--[[ hello\n\n\nworld\n\nthis is a test --]]'
+
+const perlResult = '=begin hello\n\n\nworld\n\nthis is a test =cut'
+
+const powerShellResult = '<# hello\n\n\nworld\n\nthis is a test #>'
+
+const pugResult = '//-\n\t hello\n\t \n\t \n\t world\n\t \n\t this is a test\n'
+
+const pythonResult = '# hello\n# \n# \n# world\n# \n# this is a test\n'
+
+const rResult = '# hello\n# \n# \n# world\n# \n# this is a test\n'
+
+const rubyResult = '=begin hello\n\n\nworld\n\nthis is a test =end'
+
+const shellScriptResult = '# hello\n# \n# \n# world\n# \n# this is a test\n'
+
+const sqlResult = '/* hello\n\n\nworld\n\nthis is a test */'
+
+const visualBasicResult = "' hello\n' \n' \n' world\n' \n' this is a test\n"
+
+module.exports = {
+  cStyleResult,
+  batchResult,
+  coffeeScriptResult,
+  dartResult,
+  dockerFileResult,
+  fSharpResult,
+  goResult,
+  luaResult,
+  perlResult,
+  powerShellResult,
+  pugResult,
+  pythonResult,
+  rResult,
+  rubyResult,
+  shellScriptResult,
+  sqlResult,
+  visualBasicResult,
+}
