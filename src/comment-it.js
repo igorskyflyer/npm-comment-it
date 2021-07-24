0353e1fbc41253cfea66d6cfcaae1e491f5e447e
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-24 15:53:47 +0200

    Add support for Dart
    fixes #1

--- src/comment-it.js
+++ src/comment-it.js
@@ -57,6 +57,13 @@ const comment = {
   /**
    * @type {CommentFormatter}
    */
+  dart: {
+    single: CommonFormatter.DoubleSlash,
+    multi: CommonFormatter.DoubleSlash,
+  },
+  /**
+   * @type {CommentFormatter}
+   */
   fSharp: {
     single: CommonFormatter.DoubleSlash,
     multi: (value) => `(*\n${value}\n*)`,
@@ -171,16 +178,6 @@ const comment = {
    */
   xml: xmlCommentFormatter,
 }
-
-console.log(
-  comment.batch.multi(`Copyright 2021 Igor.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`)
-)
 
 module.exports = {
   comment,
