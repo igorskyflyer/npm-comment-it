23201a7a2601d82aaba956dfb7d82a311558686f
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-28 00:15:41 +0200

    Aliases and API extensibility
    Fixes:
    - provide extensibility while keeping IntelliSense working (closes #26),
    - support aliases (closes #24).

--- src/comment-it.js
+++ src/comment-it.js
@@ -14,139 +14,152 @@ const xmlFormatter = require('./formatters/xmlFormatter')
  */
 
 /**
- * @typedef CommentFormatter
+ * @typedef {object} CommentFormatter Formatter
  * @property {CommentTransform} single Formats the string as a single-line comments.
  * @property {CommentTransform} multi Formats the string as a multi-line comments.
  */
 
 /**
- * @typedef Comment
- * @property {CommentFormatter} batch Batch formatter.
- * @property {CommentFormatter} coffeeScript CoffeeScript formatter.
- * @property {CommentFormatter} c C formatter.
- * @property {CommentFormatter} cpp C++ formatter.
- * @property {CommentFormatter} cSharp C# formatter.
- * @property {CommentFormatter} css CSS formatter.
- * @property {CommentFormatter} dart Dart formatter.
- * @property {CommentFormatter} delphi Delphi/Object-Pascal formatter.
- * @property {CommentFormatter} dockerFile Dockerfile formatter.
- * @property {CommentFormatter} fSharp F# formatter.
- * @property {CommentFormatter} go Go formatter.
- * @property {CommentFormatter} groovy Groovy formatter.
- * @property {CommentFormatter} html HTML formatter.
- * @property {CommentFormatter} java Java formatter.
- * @property {CommentFormatter} javaScript JavaScript formatter.
- * @property {CommentFormatter} jsx JSX formatter.
- * @property {CommentFormatter} kotlin Kotlin formatter.
- * @property {CommentFormatter} lua Lua formatter.
- * @property {CommentFormatter} matlab MATLAB formatter.
- * @property {CommentFormatter} objectiveC Objective-C formatter.
- * @property {CommentFormatter} objectiveCpp Objective-C++ formatter.
- * @property {CommentFormatter} pascal Pascal formatter.
- * @property {CommentFormatter} perl Perl formatter.
- * @property {CommentFormatter} php PHP formatter.
- * @property {CommentFormatter} powerShell PowerShell formatter.
- * @property {CommentFormatter} pug Pug/Jade formatter.
- * @property {CommentFormatter} python Python formatter.
- * @property {CommentFormatter} r R formatter.
- * @property {CommentFormatter} razor Razor formatter.
- * @property {CommentFormatter} ruby Ruby formatter.
- * @property {CommentFormatter} rust Rust formatter.
- * @property {CommentFormatter} scala Scala formatter.
- * @property {CommentFormatter} scala Scala formatter.
- * @property {CommentFormatter} shellScript ShellScript/Bash formatter.
- * @property {CommentFormatter} sql SQL formatter.
- * @property {CommentFormatter} swift Swift formatter.
- * @property {CommentFormatter} typeScript TypeScript formatter.
- * @property {CommentFormatter} vue Vue formatter.
- * @property {CommentFormatter} vueHtml Vue-HTML formatter.
- * @property {CommentFormatter} visualBasic Visual Basic formatter.
- * @property {CommentFormatter} xml XML formatter.
+ * @typedef {Object} BuiltInFormatters
+ * @property {CommentFormatter} bash ShellScript/Bash formatter
+ * @property {CommentFormatter} batch Batch formatter
+ * @property {CommentFormatter} c C formatter
+ * @property {CommentFormatter} cSharp C# formatter
+ * @property {CommentFormatter} coffeeScript CoffeeScript formatter
+ * @property {CommentFormatter} cpp C++ formatter
+ * @property {CommentFormatter} css CSS formatter
+ * @property {CommentFormatter} dart Dart formatter
+ * @property {CommentFormatter} delphi Delphi formatter
+ * @property {CommentFormatter} dockerFile Dockerfile formatter
+ * @property {CommentFormatter} fSharp F# formatter
+ * @property {CommentFormatter} go Go formatter
+ * @property {CommentFormatter} groovy Groovy formatter
+ * @property {CommentFormatter} html HTML formatter
+ * @property {CommentFormatter} java Java formatter
+ * @property {CommentFormatter} javaScript JavaScript formatter
+ * @property {CommentFormatter} jsx JSX formatter
+ * @property {CommentFormatter} kotlin Kotlin formatter
+ * @property {CommentFormatter} lua Lua formatter
+ * @property {CommentFormatter} matlab MATLAB formatter
+ * @property {CommentFormatter} objectiveC Objective-C formatter
+ * @property {CommentFormatter} objectiveCpp Objective-C++ formatter
+ * @property {CommentFormatter} pascal Pascal formatter
+ * @property {CommentFormatter} perl Perl formatter
+ * @property {CommentFormatter} php PHP formatter
+ * @property {CommentFormatter} powerShell PowerShell formatter
+ * @property {CommentFormatter} pug Pug/Jade formatter
+ * @property {CommentFormatter} python Python formatter
+ * @property {CommentFormatter} r R formatter
+ * @property {CommentFormatter} razor Razor formatter
+ * @property {CommentFormatter} ruby Ruby formatter
+ * @property {CommentFormatter} rust Rust formatter
+ * @property {CommentFormatter} scala Scala formatter
+ * @property {CommentFormatter} shellScript ShellScript/Bash formatter
+ * @property {CommentFormatter} sql SQL formatter
+ * @property {CommentFormatter} swift Swift formatter
+ * @property {CommentFormatter} typeScript TypeScript formatter
+ * @property {CommentFormatter} visualBasic VisualBasic formatter
+ * @property {CommentFormatter} vue Vue formatter
+ * @property {CommentFormatter} vueHtml VueHtml formatter
+ * @property {CommentFormatter} xml XML formatter
  */
 
 /**
- * @type {Comment}
- * Contains all formatters.
+ * @typedef {BuiltInFormatters & Record<string, CommentFormatter>} Comment
  */
-const comment = {
-  batch: {
-    single: (value) => `REM ${value}`,
-    multi: (value) => multilineFormatter(value, 'REM'),
-  },
-  coffeeScript: {
-    single: CommonFormatter.Pound,
-    multi: (value) => `###\n${value}\n###`,
-  },
-  c: cStyleFormatter,
-  cpp: cStyleFormatter,
-  cSharp: cStyleFormatter,
-  css: {
-    single: CommonFormatter.SlashAsterisk,
-    multi: CommonFormatter.SlashAsterisk,
-  },
-  dart: doubleSlashFormatter,
-  delphi: {
-    single: (value) => `{ ${value} }`,
-    multi: CommonFormatter.StarParen,
-  },
-  dockerFile: poundFormatter,
-  fSharp: {
-    single: CommonFormatter.DoubleSlash,
-    multi: CommonFormatter.StarParen,
-  },
-  go: doubleSlashFormatter,
-  groovy: cStyleFormatter,
-  html: xmlFormatter,
-  java: cStyleFormatter,
-  javaScript: cStyleFormatter,
-  jsx: jsxFormatter,
-  kotlin: cStyleFormatter,
-  lua: {
-    single: (value) => `-- ${value}`,
-    multi: (value) => `--[[ ${value} --]]`,
-  },
-  matlab: {
-    single: (value) => `% ${value}`,
-    // requires newlines !
-    multi: (value) => `%{\n${value}\n%}`,
-  },
-  objectiveC: cStyleFormatter,
-  objectiveCpp: cStyleFormatter,
-  pascal: {
-    single: (value) => `{ ${value} }`,
-    multi: CommonFormatter.StarParen,
-  },
-  perl: poundFormatter,
-  php: cStyleFormatter,
-  powerShell: {
-    single: CommonFormatter.Pound,
-    multi: (value) => `<# ${value} #>`,
-  },
-  pug: {
-    single: (value) => `//- ${value}`,
-    multi: (value) => `//-\n${multilineFormatter(value, '\t', true)}`,
-  },
-  python: poundFormatter,
-  r: poundFormatter,
-  razor: cStyleFormatter,
-  ruby: poundFormatter,
-  rust: cStyleFormatter,
-  scala: cStyleFormatter,
-  shellScript: poundFormatter,
-  sql: {
-    single: (value) => `-- ${value}`,
-    multi: CommonFormatter.SlashAsterisk,
-  },
-  swift: cStyleFormatter,
-  typeScript: cStyleFormatter,
-  vue: cStyleFormatter,
-  vueHtml: xmlFormatter,
-  visualBasic: {
-    single: (value) => `' ${value}`,
-    multi: (value) => multilineFormatter(value, "'"),
-  },
-  xml: xmlFormatter,
+
+/**
+ * Contains all formatters.
+ * @type {Comment}
+ */
+const comment = {}
+
+comment.c =
+  comment.cpp =
+  comment.cSharp =
+  comment.groovy =
+  comment.java =
+  comment.javaScript =
+  comment.kotlin =
+  comment.objectiveC =
+  comment.objectiveCpp =
+  comment.php =
+  comment.razor =
+  comment.rust =
+  comment.scala =
+  comment.swift =
+  comment.typeScript =
+  comment.vue =
+    cStyleFormatter
+
+comment.dart = comment.go = doubleSlashFormatter
+
+comment.dockerFile = comment.perl = comment.python = comment.r = comment.ruby = comment.shellScript = poundFormatter
+
+comment.html = comment.vueHtml = comment.xml = xmlFormatter
+
+comment.jsx = jsxFormatter
+
+comment.batch = { single: (value) => `REM ${value}`, multi: (value) => multilineFormatter(value, 'REM') }
+
+comment.coffeeScript = {
+  single: CommonFormatter.Pound,
+  multi: (value) => `###\n${value}\n###`,
 }
+
+comment.css = {
+  single: CommonFormatter.SlashAsterisk,
+  multi: CommonFormatter.SlashAsterisk,
+}
+
+comment.delphi = {
+  single: (value) => `{ ${value} }`,
+  multi: CommonFormatter.StarParen,
+}
+
+comment.fSharp = {
+  single: CommonFormatter.DoubleSlash,
+  multi: CommonFormatter.StarParen,
+}
+
+comment.lua = {
+  single: (value) => `-- ${value}`,
+  multi: (value) => `--[[ ${value} --]]`,
+}
+
+comment.matlab = {
+  single: (value) => `% ${value}`,
+  // requires newlines !
+  multi: (value) => `%{\n${value}\n%}`,
+}
+
+comment.pascal = {
+  single: (value) => `{ ${value} }`,
+  multi: CommonFormatter.StarParen,
+}
+
+comment.powerShell = {
+  single: CommonFormatter.Pound,
+  multi: (value) => `<# ${value} #>`,
+}
+
+comment.pug = {
+  single: (value) => `//- ${value}`,
+  multi: (value) => `//-\n${multilineFormatter(value, '\t', true)}`,
+}
+
+comment.sql = {
+  single: (value) => `-- ${value}`,
+  multi: CommonFormatter.SlashAsterisk,
+}
+
+comment.visualBasic = {
+  single: (value) => `' ${value}`,
+  multi: (value) => multilineFormatter(value, "'"),
+}
+
+// Aliases
+comment.bash = comment.shellScript
 
 /**
  * Gets IDs of all available language
@@ -188,9 +201,43 @@ function language(id) {
  * @returns {boolean}
  */
 function supportsLanguage(id) {
-  const languages = Object.keys(comment)
+  const languages = getLanguageIds()
 
   return strIsIn(id, languages, (langId, myId) => langId.toLowerCase() === myId.toLowerCase())
+}
+
+/**
+ *
+ * @param {string} id
+ * @param {string} alias
+ * @returns {boolean}
+ */
+function alias(id, alias) {
+  if (typeof id !== 'string' || typeof alias !== 'string') {
+    return false
+  }
+
+  if (id.length === 0) {
+    return false
+  }
+  if (alias.length === 0) {
+    return false
+  }
+
+  const allLanguages = getLanguageIds()
+
+  if (strIsIn(alias, allLanguages)) {
+    return false
+  }
+
+  const languageId = findMatch.full(id, allLanguages)
+
+  if (languageId.length > 0) {
+    comment[alias] = comment[languageId]
+    return true
+  }
+
+  return false
 }
 
 module.exports = {
@@ -198,4 +245,5 @@ module.exports = {
   language,
   supportsLanguage,
   getLanguageIds,
+  alias,
 }
