24c0f4542d6d4afa3357ed23588468d9b3e8af21
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-28 01:59:57 +0200

    Add LanguageId

---
+++ src/LanguageId.js
@@ -0,0 +1,8 @@
+
+// this typedef is generated automatically, do not edit it manually!
+
+/**
+ * @typedef {("bash"|"batch"|"c"|"cSharp"|"coffeeScript"|"cpp"|"css"|"dart"|"delphi"|"dockerFile"|"fSharp"|"go"|"groovy"|"html"|"java"|"javaScript"|"jsx"|"kotlin"|"lua"|"matlab"|"objectiveC"|"objectiveCpp"|"pascal"|"perl"|"php"|"powerShell"|"pug"|"python"|"r"|"razor"|"ruby"|"rust"|"scala"|"shellScript"|"sql"|"swift"|"typeScript"|"visualBasic"|"vue"|"vueHtml"|"xml")} LanguageId
+*/
+
+module.exports = {}
