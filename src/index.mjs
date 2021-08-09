a17bc315dae73f3eb7b78aceeaf9b0d58f16e2dc
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-09 19:28:59 +0200

    ESM support

---
+++ src/index.mjs
@@ -0,0 +1,23 @@
+import {
+  comment,
+  language,
+  supportsLanguage,
+  getLanguageIds,
+  alias,
+  BuiltInFormatters,
+  Comment,
+  CommentFormatter,
+  CommentTransform,
+} from './index.js'
+
+export {
+  comment,
+  language,
+  supportsLanguage,
+  getLanguageIds,
+  alias,
+  BuiltInFormatters,
+  Comment,
+  CommentFormatter,
+  CommentTransform,
+}
