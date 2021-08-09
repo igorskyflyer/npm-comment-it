68e55506f0808bea91b6a61008f83fc5a753a97d
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-09 19:26:52 +0200

    Add JSDoc types

--- src/formatters/xmlFormatter.js
+++ src/formatters/xmlFormatter.js
@@ -1,3 +1,6 @@
+/**
+ * @type {Object.<string, import('../index.js').CommentTransform>}
+ */
 module.exports = {
   single: (value) => `<!-- ${value} -->`,
   multi: (value) => `<!--\n${value}\n-->`,
