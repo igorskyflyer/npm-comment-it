69aea68d31136b78ea51a9cd419a2750a4c61ae4
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-09 19:26:31 +0200

    Add JSDoc types

--- src/formatters/doubleSlashFormatter.js
+++ src/formatters/doubleSlashFormatter.js
@@ -2,5 +2,10 @@ const { CommonFormatter, multilineFormatter } = require('./commonFormatters')
 
 module.exports = {
   single: CommonFormatter.DoubleSlash,
+  /**
+   *
+   * @param {string} value
+   * @returns {string}
+   */
   multi: (value) => multilineFormatter(value, '//'),
 }
