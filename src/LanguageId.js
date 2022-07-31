dda645ddb45890e2e6827b2b87ca33ea5f47f70a
igorskyflyer <igor.dvlpr@gmail.com>
2022-07-31 22:52:38 +0200

    * auto-generated *

--- src/LanguageId.js
+++ src/LanguageId.js
@@ -1,7 +1,7 @@
 // this typedef is generated automatically, do not edit it manually!
 
 /**
- * @typedef {("bash"|"batch"|"c"|"cSharp"|"coffeeScript"|"cpp"|"css"|"dart"|"delphi"|"dockerFile"|"fSharp"|"go"|"groovy"|"html"|"java"|"javaScript"|"jsx"|"kotlin"|"lua"|"matlab"|"objectiveC"|"objectiveCpp"|"pascal"|"perl"|"php"|"powerShell"|"pug"|"python"|"r"|"razor"|"ruby"|"rust"|"scala"|"shellScript"|"sql"|"swift"|"typeScript"|"visualBasic"|"vue"|"vueHtml"|"xml")} LanguageId
+ * @typedef {("ada"|"bash"|"batch"|"c"|"cSharp"|"coffeeScript"|"cpp"|"crystal"|"css"|"dart"|"delphi"|"dockerFile"|"elixir"|"erlang"|"euphoria"|"fSharp"|"fortran"|"genie"|"go"|"groovy"|"hack"|"haskell"|"html"|"icon"|"java"|"javaScript"|"jsx"|"julia"|"kotlin"|"lisp"|"liveCode"|"lua"|"maple"|"matlab"|"mercury"|"mql4"|"objectiveC"|"objectiveCpp"|"oz"|"pascal"|"perl"|"php"|"powerShell"|"pug"|"python"|"q"|"r"|"razor"|"red"|"ring"|"ruby"|"rust"|"scala"|"shellScript"|"sql"|"swift"|"typeScript"|"vala"|"visualBasic"|"vue"|"vueHtml"|"xml")} LanguageId
 */
 
 module.exports = {}
