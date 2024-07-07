66fc2a02ac0d3b46448d21d7aaf3caa02785ea57
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-07 22:52:23 +0200

    Remove language ID generating script

--- src/LanguageId.js
+++
@@ -1,7 +0,0 @@
-// this typedef is generated automatically, do not edit it manually!
-
-/**
- * @typedef {("ada"|"bash"|"batch"|"c"|"cSharp"|"carbon"|"coffeeScript"|"cpp"|"crystal"|"css"|"dart"|"delphi"|"dockerFile"|"elixir"|"erlang"|"euphoria"|"fSharp"|"fortran"|"genie"|"go"|"groovy"|"hack"|"haskell"|"html"|"icon"|"java"|"javaScript"|"jsx"|"julia"|"kotlin"|"lisp"|"liveCode"|"lua"|"maple"|"matlab"|"mercury"|"mql4"|"objectiveC"|"objectiveCpp"|"oz"|"pascal"|"perl"|"php"|"powerShell"|"pug"|"python"|"q"|"r"|"razor"|"red"|"ring"|"ruby"|"rust"|"scala"|"shellScript"|"sql"|"swift"|"typeScript"|"vala"|"visualBasic"|"vue"|"vueHtml"|"xml")} LanguageId
-*/
-
-module.exports = {}
