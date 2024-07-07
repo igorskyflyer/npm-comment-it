a6215e7f8e56c2a2bfbcbeb5fe84e85855b4120c
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-07 20:36:49 +0200

    Migrate to TypeScript

--- src/formatters/cStyleFormatter.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {Object.<string, import('../index.js').CommentTransform>}
- */
-const cStyleFormatter = {
-  single: (value) => `// ${value}`,
-  multi: (value) => `/* ${value} */`,
-}
-
-module.exports = cStyleFormatter
