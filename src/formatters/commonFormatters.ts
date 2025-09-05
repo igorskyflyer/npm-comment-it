625bb433ae308c7d2e9d073b2124cc3f3a714690
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-05 22:55:42 +0200

    Migrate to .ts

---
+++ src/formatters/commonFormatters.ts
@@ -0,0 +1,42 @@
+// Author: Igor Dimitrijević (@igorskyflyer)
+
+import type { CommentTransform } from '../CommentTransform.js'
+
+export const multilineFormatter = (
+  value: string,
+  prefix: string,
+  spaceNewlines = false
+): string => {
+  const lines: string[] = value.split(/[\r*\n]{1}/gm)
+  const count: number = lines.length
+  let result: string = ''
+
+  if (count === 0) {
+    return ''
+  }
+
+  for (let i = 0; i < count; i++) {
+    const line: string = lines[i]
+    let space: string = ' '
+
+    if (!spaceNewlines && line === '') {
+      space = ''
+    }
+
+    result += `${prefix}${space}${lines[i]}\n`
+  }
+
+  return result
+}
+
+export const commonFormatter: { [key: string]: CommentTransform } = {
+  ColonMulti: (value: string): string => multilineFormatter(value, ';'),
+  ColonSingle: (value: string): string => `; ${value}`,
+  DoubleDash: (value: string): string => `-- ${value}`,
+  DoubleSlash: (value: string): string => `// ${value}`,
+  Percent: (value: string): string => `% ${value}`,
+  Pound: (value: string): string => `# ${value}`,
+  SlashAsterisk: (value: string): string => `/* ${value} */`,
+  MultiPound: (value: string): string => multilineFormatter(value, '#'),
+  StarParen: (value: string): string => `(* ${value} *)`
+}
